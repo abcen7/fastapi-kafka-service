55144e792030b58ebb717efac2d1d422be36f75a
Kirill Sermyagin <sermyagin.abcen7@gmail.com>
2025-01-20 13:02:12 +0300

    Removed wrappers of kafka

--- app/users/services.py
+++ app/users/services.py
@@ -1,21 +1,17 @@
-import asyncio
 import json
-import random
 import time
 import uuid
-from decimal import Decimal
 from typing import Sequence
 
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+
 from ..core import settings
-from ..core.kafka.consumer import AIOWebConsumer
-from ..core.kafka.producer import AIOWebProducer
 from ..core.lib import main_logger
 from .models import User
 from .repositories import UsersRepository
 from .schemas import CryptoServiceType, UserCreate, UserDataRequest, UserDataResponse
 
 
-# TODO: add logging
 class UsersService:
     repository = UsersRepository()
 
@@ -26,91 +22,70 @@ class UsersService:
         return await self.repository.get_all()
 
     @classmethod
-    async def simulate_process_messages(cls) -> None:
-        """Receives and processes messages from Kafka topic."""
-        main_logger.info("Started simulates processing messages")
-        consumer = AIOWebConsumer(consume_topic=settings.kafka.PRODUCE_TOPIC)
-        producer = AIOWebProducer(produce_topic=settings.kafka.CONSUME_TOPIC)
-        await consumer.start()
-        await producer.start()
-        count_sent_messages = 0
-        try:
-            async for message in await consumer.get():
-                decoded_message = json.loads(message.value)
-                user_data_request = UserDataRequest(**decoded_message)
-                main_logger.info(
-                    f"Received message from balances_request >> {user_data_request}"
-                )
-                for service_type in CryptoServiceType:
-                    response = UserDataResponse(
-                        correlation_id=user_data_request.correlation_id,
-                        produced_by=service_type,
-                        user_id=user_data_request.user_id,
-                        balance=Decimal(random.uniform(1.0, 100.0)),
-                    )
-                    await producer.send(
-                        json.dumps(response.model_dump()).encode("ascii")
-                    )
-                    count_sent_messages += 1
-                    main_logger.info(f"Sent message to balances_response >> {response}")
-                if count_sent_messages == len(CryptoServiceType):
-                    main_logger.info("Finished simulates processing messages")
-                    return
-        except Exception as e:
-            main_logger.error(f"Error in simulates processing messages: {str(e)}")
-            raise e
-        finally:
-            await consumer.stop()
-            await producer.stop()
-
-    async def simulate_send_messages(self, user_id: int) -> None:
+    async def _send_messages(cls, user_id: int, correlation_id: uuid.UUID) -> None:
         """Writes to balances_request topic, requesting user info"""
-        producer = AIOWebProducer()
-        await producer.start()
         try:
-            main_logger.info("Started simulates sending messages")
-            request = UserDataRequest(
-                correlation_id=uuid.uuid4(),
-                user_id=user_id,
-            )
-            await producer.send(json.dumps(request.model_dump()).encode("ascii"))
-            main_logger.info(f"Sent message to balances_request >> {request}")
-            main_logger.info("Finished simulates sending messages")
-            return None
+            async with AIOKafkaProducer(
+                bootstrap_servers=settings.kafka.BROKER,
+            ) as producer:
+                main_logger.info("Started sending messages")
+                request = UserDataRequest(
+                    correlation_id=correlation_id,
+                    user_id=user_id,
+                )
+                await producer.send(
+                    value=json.dumps(request.model_dump()).encode("ascii"),
+                    topic=settings.kafka.PRODUCE_TOPIC,
+                )
+                main_logger.info(f"Sent message to balances_request >> {request}")
+                main_logger.info("Finished sending messages")
+                return None
         except Exception as e:
-            main_logger.error(f"Error in simulates sending messages: {str(e)}")
+            main_logger.error(f"Error in sending messages: {str(e)}")
             raise e
-        finally:
-            await producer.stop()
 
     async def get_all_info(self, user_id: int) -> float:
         """Consumes messages from balances_response"""
-        consumer = AIOWebConsumer()
-        await consumer.start()
-
-        count_waiting_messages = len(CryptoServiceType)
-        threshold_time = 5
+        count_waiting_messages = len(
+            CryptoServiceType
+        )  # Count of all requesting services
+        threshold_time = 5  # Threshold time in seconds when we are going to stop wait the messages from requested services
         balance = 0
         count_messages_received = 0
         start_time = time.time()
+        request_correlation_id = uuid.uuid4()
 
-        process_task = asyncio.create_task(self.simulate_process_messages())
-        await self.simulate_send_messages(user_id)
-        await process_task
         try:
-            async for message in await consumer.get():
-                decoded_message = UserDataResponse(**json.loads(message.value))
-                main_logger.info(
-                    f"Received message from balances_response >> {decoded_message}"
-                )
-                balance += decoded_message.balance
-                count_messages_received += 1
-                if count_messages_received == count_waiting_messages:
+            async with AIOKafkaConsumer(
+                settings.kafka.CONSUME_TOPIC,
+                bootstrap_servers=settings.kafka.BROKER,
+            ) as consumer:
+                await self._send_messages(user_id, request_correlation_id)
+
+                # TODO: need to understand how to exit from async for cycle, when consumer queue is empty
+                if (
+                    count_messages_received < count_waiting_messages
+                    and time.time() - start_time > threshold_time
+                ):
                     return balance
-                if time.time() - start_time > threshold_time:
+
+                async for message in consumer:
+                    decoded_message = UserDataResponse(**json.loads(message.value))
+                    main_logger.info(
+                        f"Received message from balances_response >> {decoded_message}"
+                    )
+                    balance += decoded_message.balance
+                    count_messages_received += 1
+                    if count_messages_received == count_waiting_messages:
+                        return balance
+                    if time.time() - start_time > threshold_time:
+                        return balance
+                if (
+                    count_messages_received < count_waiting_messages
+                    and time.time() - start_time > threshold_time
+                ):
                     return balance
+            return balance
         except Exception as e:
             main_logger.error(f"Error in consuming messages in get_all_info: {str(e)}")
             raise e
-        finally:
-            await consumer.stop()
--- example_microservice.py
+++ example_microservice.py
@@ -3,50 +3,46 @@ import json
 import random
 from decimal import Decimal
 
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+
 from app.core import settings
-from app.core.kafka.consumer import AIOWebConsumer
-from app.core.kafka.producer import AIOWebProducer
 from app.core.lib import main_logger
 from app.users.schemas import CryptoServiceType, UserDataRequest, UserDataResponse
 
 
 async def simulate_process_messages() -> None:
-    """
-    Receives and processes messages from Kafka topic.
-    """
+    """Receives and processes messages from Kafka topic."""
     main_logger.info("Started simulates processing messages")
-    consumer = AIOWebConsumer(consume_topic=settings.kafka.PRODUCE_TOPIC)
-    producer = AIOWebProducer(produce_topic=settings.kafka.CONSUME_TOPIC)
-    await consumer.start()
-    await producer.start()
     try:
-        async for message in await consumer.get():
-            decoded_message = json.loads(message.value)
-            main_logger.info(f"Decoded message: {decoded_message}")
-            user_data_request = UserDataRequest(**decoded_message)
-            for service_type in CryptoServiceType:
-                response = UserDataResponse(
-                    correlation_id=user_data_request.correlation_id,
-                    produced_by=service_type,
-                    user_id=user_data_request.user_id,
-                    balance=Decimal(random.uniform(1.0, 100.0)),
-                )
-                main_logger.info(f"Response: {response}")
-                try:
-                    await producer.send(
-                        json.dumps(response.model_dump()).encode("ascii")
-                    )
-                except Exception as send_error:
-                    main_logger.error(f"Error sending message: {send_error}")
-                main_logger("The message was sent successfully")
-        main_logger.info("Finished simulates processing messages")
-        return None
+        async with AIOKafkaConsumer(
+            settings.kafka.PRODUCE_TOPIC,
+            bootstrap_servers=settings.kafka.BROKER,
+        ) as consumer:
+            async with AIOKafkaProducer(
+                bootstrap_servers=settings.kafka.BROKER,
+            ) as producer:
+                async for message in consumer:
+                    decoded_message = json.loads(message.value)
+                    main_logger.info(f"Decoded message: {decoded_message}")
+                    user_data_request = UserDataRequest(**decoded_message)
+                    for service_type in CryptoServiceType:
+                        response = UserDataResponse(
+                            correlation_id=user_data_request.correlation_id,
+                            produced_by=service_type,
+                            user_id=user_data_request.user_id,
+                            balance=Decimal(random.uniform(1.0, 100.0)),
+                        )
+                        main_logger.info(f"Response: {response}")
+                        await producer.send(
+                            value=json.dumps(response.model_dump()).encode("ascii"),
+                            topic=settings.kafka.CONSUME_TOPIC,
+                        )
+                        main_logger.info("The message was sent successfully")
+                main_logger.info("Finished simulates processing messages")
+                return None
     except Exception as e:
         main_logger.error(f"Error in simulates processing messages: {str(e)}")
         raise e
-    finally:
-        await consumer.stop()
-        await producer.stop()
 
 
 def main():
